c24784effadf63c10c684137414edd134d67a663
ErrorInever <errorinever@gmail.com>
2021-09-12 03:24:45 +0000

    fix transform

--- dataset.py
+++ dataset.py
@@ -2,16 +2,21 @@ import torch
 import numpy as np
 from scipy import signal
 from torch.utils.data import Dataset
+from nnAudio.Spectrogram import CQT1992v2
+from config import cfg
 
 
 class GWDataset(Dataset):
     """Gravitation Wave Dataset"""
-    def __init__(self, df, use_filter=True):
+    def __init__(self, df, use_filter=True, use_transform=True):
         self.df = df
         self.id_series = df['id'].values
         self.paths_series = df['img_path'].values
         self.targets = df['target'].values
         self.use_filter = use_filter
+        self.wave_transform = CQT1992v2(**cfg.Q_TRANSFORM_PARAMS)
+        self.transform = use_transform
+
         if self.use_filter:
             self.bHP, self.aHP = signal.butter(8, (20, 500), btype='bandpass', fs=2048)
 
@@ -27,5 +32,10 @@ class GWDataset(Dataset):
             series = signal.filtfilt(self.bHP, self.aHP, series)
         # Normalize
         series = series / np.max(series)
+        # Q-Transform
+        if self.transform:
+            series = torch.from_numpy(series).float()
+            series = self.wave_transform(series).squeeze()
+            series = series.squeeze().numpy()
         targets = self.targets[idx]
         return torch.tensor(series, dtype=torch.float), torch.tensor(targets, dtype=torch.long)
